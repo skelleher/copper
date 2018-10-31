b84f0cb5219fb91b8d5b6a2b07853b4fa3642af9
Sean Kelleher <sean@seankelleher.com>
2018-10-31 11:45:55 -0700

    fix whitespace error

--- resnet.py
+++ resnet.py
@@ -178,8 +178,8 @@ class ResnetCBIR( nn.Module ):
         self.avgpool = torch.nn.AvgPool2d( (7,7) ) # Bx7x7 features -> Bx7x7 avgpool == Bx1x1 output
         self.reshape = View( 2048 )
         
-         # final layer must be named .fc, so that model.class_table = <foo> can find it and resize it
-         self.fc = nn.Linear(2048, self._embedding_length)
+        # final layer must be named .fc, so that model.class_table = <foo> can find it and resize it
+        self.fc = nn.Linear(2048, self._embedding_length)
 
         print( "Size after embedding layer: ", self._embedding_length )
 
